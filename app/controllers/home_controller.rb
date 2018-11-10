a2d437254282eb76262dcbd8c8b84feb084b6c76
rsbrown <sbrown@greenbits.com>
2018-11-09 20:46:40 -0800

    show uploaded tilesets programmatically

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+
+  def index
+    @tileset = Tileset.last
+    redirect_to('/upload', flash: { alert: "No tilesets. Please upload one." }) if @tileset.nil?
+  end
+
 end
